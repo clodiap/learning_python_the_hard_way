7cd0a1c49446d3cb8115584827ab920fd667ab19
clodiap <clodiapp@gmail.com>
2018-08-10 13:39:06 +0200

    exercise 9, printing printing printing

---
+++ ex09.py
@@ -0,0 +1,21 @@
+# Printing, printing, printing
+
+# Here's some new strange stuff, remember type it exactly
+
+# define a string variable
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# define a string variable with new lines
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# print text + the variables to the screen
+print("Here are the days:", days)
+print("Here are the months:", months)
+
+# print text on several lines
+print("""
+There's something going on here.
+
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
