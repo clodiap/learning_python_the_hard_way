173e57b5f2fed08cebf27f35f42db6ef4328652d
clodiap <clodiapp@gmail.com>
2018-08-21 22:35:15 +0200

    exercise 23 suite

--- ex23.py
+++ ex23.py
@@ -4,6 +4,7 @@ import sys
 
 script, input_encoding, error = sys.argv
 
+# function that opens and read the file
 def main(language_file, encoding, errors):
     line = language_file.readline()
 
@@ -11,6 +12,7 @@ def main(language_file, encoding, errors):
         print_line(line, encoding, errors)
         return main(language_file, encoding, errors)
 
+# function called by the first one, which encode and show the lines of the file
 def print_line(line, encoding, errors):
     next_lang = line.strip()
     raw_bytes = next_lang.encode(encoding, errors=errors)
